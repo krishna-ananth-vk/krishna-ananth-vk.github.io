1a694b16cbd88ac86d1dd66e02a3a0ba0cdadce3
Ananthakrishnan VK <krishnaananthvk@gmail.com>
2024-08-30 01:39:48 +0530

    ui changes and updated resume

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const elements = ['Developer','Designer'];
-let i = 0 ;
+const elements = ['Developer', 'Designer'];
+let i = 0;
 let count = 0;
 let currentword = '';
 let letter = '';
@@ -11,157 +11,87 @@ let letter = '';
 // });
 
 function typewrite() {
-    if(count === elements.length){
+    if (count === elements.length) {
         count = 0;
     }
     currentword = elements[count];
-    if(i<currentword.length ){
+    if (i < currentword.length) {
         document.getElementById("typewrite").innerHTML += currentword.charAt(i);
         i++;
-        setTimeout(typewrite,250);
+        setTimeout(typewrite, 250);
     }
-    else{
+    else {
         count++;
-        i=0;
+        i = 0;
         document.getElementById("typewrite").innerHTML = '';
-        setTimeout(typewrite,500);
+        setTimeout(typewrite, 500);
     }
-    
+
 }
 
 
-
-
-
-
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
-
-
-
-
-
-
-
-
-
-function change(x){
+function change(x) {
     x.classList.toggle("change")
     $(".nav-menu").toggleClass("change")
 }
 
-function hide(i){
+function hide(i) {
     $(".nav-menu").toggleClass("change");
     $(".menu").toggleClass("change");
-    switch(i){
-        case 'projects': 
-                        console.log(i)
-                        var  p =  document.getElementById("proj");
-                        p.scrollIntoView();
-                        console.log(p)
-                        break;
-        case 'contacts': 
-                        console.log(i)
-                        var  c =  document.getElementById("contact");
-                        c.scrollIntoView();
-                        console.log(c)
-                        break;
+    switch (i) {
+        case 'projects':
+            console.log(i)
+            var p = document.getElementById("proj");
+            p.scrollIntoView();
+            console.log(p)
+            break;
+        case 'contacts':
+            console.log(i)
+            var c = document.getElementById("contact");
+            c.scrollIntoView();
+            console.log(c)
+            break;
 
     }
 }
 
-function myself(){
-    
-    var  v =  document.getElementById("myse");
+function myself() {
+
+    var v = document.getElementById("myse");
     v.scrollIntoView();
     console.log(v)
 }
 
 
-function sendMail(){
+function sendMail() {
     var name = document.getElementById("name").value;
     var mail = document.getElementById("mail").value;
     var msg = document.getElementById("msg").value;
-    if(name=='' || mail=='' || msg==''){
-        
-        if(name==''){
-            $("#name").attr('placeholder',"please enter your name..!")
+    if (name == '' || mail == '' || msg == '') {
+
+        if (name == '') {
+            $("#name").attr('placeholder', "please enter your name..!")
         }
 
-        if( mail=='' ){
-            $("#mail").attr('placeholder',"please enter your mail ID..!")
+        if (mail == '') {
+            $("#mail").attr('placeholder', "please enter your mail ID..!")
         }
 
-        if(msg==''){
-            $("#msg").attr('placeholder',"please enter message..!")
+        if (msg == '') {
+            $("#msg").attr('placeholder', "please enter message..!")
         }
-        
+
     }
-    else{
+    else {
         ument.getElementById("btn").innerHTML = "Sending....";
         document.getElementById("btn").disabled = true;
-        
-        document.getElementById("name").value='';
-        document.getElementById("mail").value='';
-        document.getElementById("msg").value='';
+
+        document.getElementById("name").value = '';
+        document.getElementById("mail").value = '';
+        document.getElementById("msg").value = '';
 
     }
 
-    
+
 
 }
